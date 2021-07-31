425a97d1751c0065553ebafae32863e2a0ee9b55
Angelo Michael Huaraca Berrospi <ahuaraca@everis.com>
2021-07-30 19:12:12 -0500

    first commit

---
+++ cypress/integration/todomvc-actions.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="cypress" />
+import {
+    navigate,
+    addTodo,
+    validateTodoText,
+    validateToggleState,
+    toggleTodo,
+    validateTodoCompletedState,
+    clearCompleted,
+    validateNumberOfTodosShown
+} from "../page-objects/todo-page";
+
+describe('todo actions', () => {
+
+    beforeEach(() => {
+        navigate()
+        addTodo('Clean room')
+    })
+
+    it('Should be able to add new todo to the list', () => {
+        validateTodoText(0, 'Clean room')
+        validateToggleState(0, false)
+    })
+
+    describe('toggling todos', () => {
+
+        it('should toggle test correctly', () => {
+            toggleTodo(0)
+            validateTodoCompletedState(0, true)
+        })
+
+        it('Should clear completed', () => {
+            toggleTodo(0)
+            clearCompleted()
+            validateNumberOfTodosShown(0)
+        })
+    })
+})
